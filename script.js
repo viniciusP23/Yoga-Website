94beba118d3c4ad892c35dc95f8b8469f6743a4f
viniciusP23 <158086630+viniciusP23@users.noreply.github.com>
2025-12-24 16:51:09 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -13,3 +13,40 @@ navLinks.addEventListener("click", () => {
     navLinks.classList.remove("open")
     menuBtnIcon.setAttribute("class", "ri-menu-4-line")
 })
+
+const scrollRevealOption = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
+}
+
+ScrollReveal().reveal(".header-image img", {
+    ...scrollRevealOption,
+    origin: "right",
+})
+
+ScrollReveal().reveal(".header-content h2", {
+    ...scrollRevealOption,
+    delay: 500,
+})
+
+ScrollReveal().reveal(".header-content h1", {
+    ...scrollRevealOption,
+    delay: 1000,
+})
+
+ScrollReveal().reveal(".header-content p", {
+    ...scrollRevealOption,
+    delay: 1500,
+})
+
+ScrollReveal().reveal(".header-btn", {
+    ...scrollRevealOption,
+    delay: 2000,
+})
+
+ScrollReveal().reveal(".header-socials li", {
+    ...scrollRevealOption,
+    delay: 2500,
+    interval: 500,
+})
